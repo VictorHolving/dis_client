a216b05464761c2b94476662bfe6131c017e8d01
VictorHolving <vholving@gmail.com>
2015-10-29 11:43:46 +0100

    Metode og variabler tilføjet

--- src/Logic.java
+++ src/Logic.java
@@ -1,6 +1,25 @@
 /**
  * Created by Victor on 29/10/15.
  */
+
+package sdk;
+
+import com.google.gson.Gson;
+
 public class Logic {
+    static ServerConnection serverConnection = new ServerConnection();
+
+    public static void login(String username, String password){
+
+        User user = new User();
+        user.setPassword(password);
+        user.setUsername(username);
+
+        String json = new Gson().toJson(user);
+
+        serverConnection.post(json, "login/");
+
+    }
+    }
 
 }
